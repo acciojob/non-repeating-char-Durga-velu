5e4dec1eaed8c6e61a8c7f950ccd9339f10f16d5
Durga-velu <durgadevi134babu@gmail.com>
2025-03-12 23:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+let res = null;
+for (let char of str) {
+    if (
+        str.indexOf(char)===str.lastIndexOf(char) {
+        res = char;
+        break;
+    }
+}
+	return res
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
